a1f444e491ce541dd9d548197ac975e74ce2b749
edenislamova <edenislamova13@gmail.com>
2017-08-15 22:31:15 +0500

    Добавлен метод distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenPoints.java
@@ -3,14 +3,11 @@ package ru.stqa.pft.sandbox;
 public class DistanceBetweenPoints {
 
   public static void main(String[] args) {
-    Point p1 = new Point(1,2);
-    Point p2 = new Point(3,4);
+    Point p1 = new Point(1,6);
+    Point p2 = new Point(4,15);
     System.out.println("Точка A с координатами" + " x = " + p1.x + " y = " + p1.y);
     System.out.println("Точка B с координатами" + " x = " + p2.x + " y = " + p2.y);
-    System.out.println("Расстояние между точками A и B: " + distance(p1 , p2));
+    System.out.println("Расстояние между точками A и B: " + p1.distance(p2.x,p2.y));
     }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow(p2.x - p1.x,2) + Math.pow(p2.y - p1.y,2));
-    }
 }
