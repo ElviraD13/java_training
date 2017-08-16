139f880fa24285f5993022a8723fe2482258c63a
edenislamova <edenislamova13@gmail.com>
2017-08-16 21:07:09 +0500

    Исправления к заданию №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenPoints.java
@@ -3,11 +3,11 @@ package ru.stqa.pft.sandbox;
 public class DistanceBetweenPoints {
 
   public static void main(String[] args) {
-    Point p1 = new Point(1,6);
-    Point p2 = new Point(4,15);
+    Point p1 = new Point(13,24);
+    Point p2 = new Point(62,87);
     System.out.println("Точка A с координатами" + " x = " + p1.x + " y = " + p1.y);
     System.out.println("Точка B с координатами" + " x = " + p2.x + " y = " + p2.y);
-    System.out.println("Расстояние между точками A и B: " + p1.distance(p2.x,p2.y));
+    System.out.println("Расстояние между точками A и B: " + p1.distance(p2));
     }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,7 +7,7 @@ public class Point {
     this.x = x;
     this.y = y;
   }
-  public double distance(double x, double y) {
-    return Math.sqrt(Math.pow(x - this.x,2) + Math.pow(y - this.y,2));
+  public double distance(Point p2) {
+    return Math.sqrt(Math.pow(p2.x - this.x,2) + Math.pow(p2.y - this.y,2));
   }
 }
