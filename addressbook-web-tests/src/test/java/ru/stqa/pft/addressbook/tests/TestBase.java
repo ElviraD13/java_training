155f868b3bf1507e700f0f41587a1bd2d6cc4844
edenislamova <edenislamova13@gmail.com>
2017-10-01 16:51:38 +0500

    ORM

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -29,11 +29,11 @@ public class TestBase {
     app.stop();
   }
 
-  @BeforeMethod(enabled = false)
+  @BeforeMethod
   public void logTestStart(Method m, Object[] p){
     logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
   }
-  @AfterMethod(alwaysRun = true,enabled = false)
+  @AfterMethod(alwaysRun = true)
   public void logTestStop(Method m){
     logger.info("Stop test " + m.getName());
   }
